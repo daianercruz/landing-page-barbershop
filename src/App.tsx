732fbf39074572b17a19fb6392ac58b74201d30b
Daiane Cruz <daianercruz12@gmail.com>
2024-03-24 20:38:24 -0300

    iniciando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import './App.css'
+import './App.scss'
+function App() {
+ 
+
+  return (
+   
+    
+
+<main>
+    
+<div className="main-container">
+
+      
+      <div className="landing-page">
+        <header>
+          <div className="container">
+            <ul className="links">
+             
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Services</a></li>
+            <li><a href="/">Contact</a></li>
+            
+              <li></li>
+            </ul>
+          </div>
+        </header>
+        <div className="content">
+          <div className="container">
+            <div className="info">
+              <h1>Barbearia Felix</h1>
+              <p>Fundada em 1998, destaque na cidade e conquista novos clientes a cada dia.
+Na Barbearia Felix, não apenas cortamos cabelo e barba, nós transformamos vidas. 
+Estamos aqui para muito mais do que apenas um serviço, estamos aqui para elevar a confiança e o bem-estar de 
+cada cliente que confia em nossos serviços. Venha fazer parte dessa experiência única e saia não apenas com um
+ visual renovado, mas com uma nova perspectiva de si mesmo.
+ Nossa missão é: "Proporcionar auto-estima e qualidade de vida aos clientes" </p>
+              <button><a href="/">Services</a></button>
+            </div>
+            <div className="image">
+            <img src='img/felix-barbershop.jpeg'></img>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+
+
+
+    <footer> 
+
+<a href="https://github.com/daianercruz"><i className="bi bi-github"></i></a>
+<a href="https://www.linkedin.com/in/daianeprc/"><i className="bi bi-linkedin"></i></a>
+
+</footer>
+</main>
+ 
+
+  )
+}
+
+export default App
